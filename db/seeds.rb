95ce53fc45b332671134621a400f62c6cb3c766a
rjmintn <rjmorawski@gmail.com>
2017-09-01 22:57:13 -0400

    seed data working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.all
+
+6.times do |count|
+  list = List.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.sentence,
+    user_id: users.sample.id
+  )
+  list.update_attributes(:created_at => Faker::Date.between(6.days.ago, Date.today), :updated_at => Date.today)
+end
+
+@lists = List.all
+
+@lists.each do |list|
+  rand(2..5).times do
+    item = Item.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.sentence,
+    user_id: list[:user_id],
+    create_by_id: list[:user_id],
+    list_id: list[:id],
+    priority: rand(1..5),
+    duration: rand(1..8),
+    repeat: Date::DAYNAMES.sample,
+    due: Faker::Date,
+    active: Faker::Date,
+    start: Faker::Date,
+
+    )
+    item.update_attributes(:created_at => Faker::Date.between(6.days.ago, Date.today),  :updated_at => Date.today)
+
+
+  end
+end
